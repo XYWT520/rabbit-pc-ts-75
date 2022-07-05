2810d36f6fa6e2d9fd2cc439f90b2819883b1c79
ZYC <2095921712@qq.com>
2022-07-05 16:33:11 +0800

    feat:导航栏

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup name="Layout">
 // 导入组件
 import AppTopnav from './components/app-topnav.vue';
+import AppHeader from './components/app-header.vue';
 </script>
 <template>
   <AppTopnav />
+  <AppHeader />
 </template>
 
 <style lang="less" scoped></style> 
